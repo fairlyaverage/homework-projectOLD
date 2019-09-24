068d21368d602908b1cbc3f5637b98e31e463710
elliott <nschwrzm@memphis.edu>
2019-09-24 18:43:54 -0500

    Added nschwrzm and software-reviews pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,15 @@
 class PagesController < ApplicationController
+
+    def nschwrzm
+        respond_to do |format|
+            format.html { render :nschwrzm }
+        end
+    end
+
+    def software_review
+        respond_to do |format|
+            format.html { render :software_review }
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'nschwrzm', to: 'pages#nschwrzm', as: 'nschwrzm'
+  get 'software_review', to: 'pages#software_review', as: 'software_review'
+
 end
